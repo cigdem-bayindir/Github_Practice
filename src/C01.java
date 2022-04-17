0a7c0dc4464d9dd00f32a08cb0afd4d6150d7f6d
cigdem <cigdemceylan001@gmail.com>
2022-04-18 01:47:49 +0300

    18.04.2022

--- src/C01.java
+++ src/C01.java
@@ -2,6 +2,7 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("Lokalden Merhaba");
         System.out.println("çiğdem branchi oluştu");
-
+        System.out.println("çiğdem branchi tekrar oluştu");
+        System.out.println("Lütfiyeye selamlar");
     }
 }
