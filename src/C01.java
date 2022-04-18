2762797cae1f209820aa612670fae588af843925
cigdem <cigdemceylan001@gmail.com>
2022-04-18 15:33:58 +0300

    sorun çözüldü

--- src/C01.java
+++ src/C01.java
@@ -1,9 +1,5 @@
 public class C01 {
     public static void main(String[] args) {
-        System.out.println("Lokalden Merhaba");
-        System.out.println("çiğdem branchi oluştu");
-        System.out.println("çiğdem branchi tekrar oluştu");
-        System.out.println("Lütfiyeye selamlar");
-        System.out.println("Github seni yenicem");
+
     }
 }
--- src/C03.java
+++ src/C03.java
@@ -1,2 +1,13 @@
 public class C03 {
-}
+    public static void main(String[] args) {
+        System.out.println("Lokalden Merhaba");
+        System.out.println("çiğdem branchi oluştu");
+
+
+        System.out.println("çiğdem branchi tekrar oluştu");
+        System.out.println("Lütfiyeye selamlar");
+        System.out.println("Github seni yenicem");
+    }
+    }
+
+
